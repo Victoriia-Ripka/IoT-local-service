ebe3f652c3ee930c528d76cd0c615538464cb726
Viktoriia Nowotka <98872709+Victoriia-Ripka@users.noreply.github.com>
2025-03-13 12:19:16 +0100

    updated server

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(express.json()); 
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.post('/data', (req, res) => {
-    res.send('Some data here');
+app.post('/api/data', (req, res) => {
+    console.log('Отримано запит:', req.body);
+
+    if (!req.body.current) {
+        return res.status(400).send({ error: "Missing 'current' field" });
+    }
+
+    console.log(`Струм: ${req.body.current} A`);
+    res.send({ message: 'Дані отримані!', received: req.body });
 });
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server is running on port 3000');
 });
 
 
